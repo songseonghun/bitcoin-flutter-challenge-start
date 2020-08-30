f5019fd9f27ec840c2dedfd74ae56eb6f9d89dae
songsh <songsh99@gmail.com>
2020-08-31 04:49:32 +0900

    commit udemy practice bronze.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 //TODO: Add your imports here.
 
 const List<String> currenciesList = [
@@ -30,9 +33,41 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async{
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+
+    } else {
+      print(response.statusCode);
+    }
+
+  }
+
+}
+
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = 'C52B1C86-9A1C-4474-A262-85FB86069B6C';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+ Future<dynamic> getCoinData() async{
+   String url;
+   url = '$coinAPIURL/BTC/USD/?apikey=$apiKey';
+
+   NetworkHelper networkHelper = NetworkHelper(url);
+
+   var coinData = await networkHelper.getData();
+
+   return coinData;
+ }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,6 +10,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  String bitCoinRate;
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -49,11 +50,31 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   //TODO: Create a method here called getData() to get the coin data from coin_data.dart
+  void getData() async {
+    CoinData coinData = CoinData();
+    var getCoinData = await coinData.getCoinData();
+    updateUI(getCoinData);
+  }
+
+  void updateUI(dynamic getCoinData) async {
+    setState(() {
+      if (getCoinData == null) {
+        bitCoinRate = '';
+        return;
+      }
+      double rateValue = getCoinData['rate'];
+      print (rateValue);
+      bitCoinRate = rateValue.toStringAsFixed(1);
+      print(bitCoinRate);
+
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     //TODO: Call getData() when the screen loads up.
+    getData();
   }
 
   @override
@@ -78,7 +99,7 @@ class _PriceScreenState extends State<PriceScreen> {
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
                   //TODO: Update the Text Widget with the live bitcoin data here.
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitCoinRate USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
